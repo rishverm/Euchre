1712d47c515f127d4cc3284bb0bb66272fd68560
jelwing <jelwing@umich.edu>
2022-02-08 12:56:06 -0500

    random commit

---
+++ Player.cpp
@@ -0,0 +1,45 @@
+//
+//  Project UID 1d9f47bfc76643019cfbf037641defe1
+//  Player.cpp
+//  p3-euchre
+//
+//  Created by Jenna Elwing on 2/7/22.
+//
+
+#include <stdio.h>
+#include <string>
+#include "Card.h"
+#include "Player.h"
+
+const std::string & get_name() {
+    assert(false);
+}
+
+void add_card(const Card &c) {
+    assert(false);
+}
+
+bool make_trump(const Card &upcard, bool is_dealer,
+                int round, std::string &order_up_suit) {
+    assert(false);
+}
+
+void add_and_discard(const Card &upcard) {
+    assert(false);
+}
+
+Card lead_card(const std::string &trump) {
+    assert(false);
+}
+
+Card play_card(const Card &led_card, const std::string &trump) {
+    assert(false);
+}
+
+Player * Player_factory(const std::string &name, const std::string &strategy) {
+    assert(false);
+}
+
+std::ostream & operator<<(std::ostream &os, const Player &p) {
+    assert(false);
+}
--- Player_tests.cpp
+++ Player_tests.cpp
@@ -7,11 +7,43 @@
 
 using namespace std;
 
-TEST(test_player_get_name) {
+TEST(test_player_get_name_basic) {
     Player * alice = Player_factory("Alice", "Simple");
     ASSERT_EQUAL("Alice", alice->get_name());
 
     delete alice;
+}
+
+TEST(test_player_get_name_2) {
+  // Create a player and verify that get_name() returns the player's name
+  Player * Gandolf = Player_factory("Gandolf", "Simple");
+  ASSERT_EQUAL(Gandolf->get_name(), "Gandolf");
+  delete Gandolf;
+}
+
+TEST(test_player_add_card_basic) {
+    Card c(Card::RANK_QUEEN, Card::SUIT_DIAMONDS);
+    
+}
+
+TEST(test_player_make_trump_basic) {
+    
+}
+
+TEST(test_player_add_and_discard_basic) {
+    
+}
+
+TEST(test_player_lead_card_basic) {
+    
+}
+
+TEST(test_player_play_card_basic) {
+    
+}
+
+TEST(test_player_player_factory_basic) {
+    
 }
 
 // Add more tests here
