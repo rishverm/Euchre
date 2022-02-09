739feedef6625c2da0181d99ee8bca4ab4a70f62
jelwing <jelwing@umich.edu>
2022-02-09 12:42:32 -0500

    Card.cpp implementation

--- Card.cpp
+++ Card.cpp
@@ -29,98 +29,399 @@ constexpr const char* const Card::SUIT_DIAMONDS;
 
 // add your code below
 Card::Card(const string& rank_in, const string& suit_in) {
-
+    rank = rank_in;
+    suit = suit_in;
 }
 
 
 Card::Card() :rank(RANK_TWO), suit(SUIT_SPADES) {
-
+    rank = RANK_TWO;
+    suit = SUIT_SPADES;
 }
 
 
 
 string Card::get_rank() const {
-	return "";
+	return rank;
 }
 
 
 string Card::get_suit() const {
-	return "";
+	return suit;
 }
-
+/*if ((trump == SUIT_DIAMONDS) && (suit == SUIT_HEARTS) && (rank == RANK_JACK)) {
+    return SUIT_DIAMONDS;
+}
+else if ((trump == SUIT_HEARTS) && (suit == SUIT_DIAMONDS) && (rank == RANK_JACK)) {
+    return SUIT_HEARTS;
+}
+else if ((trump == SUIT_CLUBS) && (suit == SUIT_SPADES) && (rank == RANK_JACK)) {
+    return SUIT_CLUBS;
+}
+else if ((trump == SUIT_SPADES) && (suit == SUIT_CLUBS) && (rank == RANK_JACK)) {
+    return SUIT_SPADES;
+}
+ */
 string Card::get_suit(const string& trump) const {
-	return "";
+    if (Card::is_left_bower(trump)) {
+        return trump;
+    }
+    else {
+        return suit;
+    }
 }
 
 
 bool Card::is_face() const {
-	return 0;
+    if (rank == RANK_JACK || rank == RANK_QUEEN) {
+        return true;
+    }
+    else if (rank == RANK_KING || rank == RANK_ACE) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 
 bool Card::is_right_bower(const string& trump) const {
-	return 0;
+    if ((rank == RANK_JACK) && (suit == trump)) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 
 bool Card::is_left_bower(const string& trump) const {
-	return 0;
+    if ((rank == RANK_JACK) && Suit_next(suit) == trump) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
-
-
 bool Card::is_trump(const string& trump) const {
-	return 0;
+    if (suit == trump) {
+        return true;
+    }
+    else if (Card::is_left_bower(trump)) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 
 bool operator<(const Card& lhs, const Card& rhs) {
-	return 0;
-	
+    int lhsValue = 0;
+    int rhsValue = 0;
+    if (lhs.get_rank() != rhs.get_rank()) {
+    
+        for (int i = 0; i < NUM_RANKS; ++i) {
+            if (RANK_NAMES_BY_WEIGHT[i] == lhs.get_rank()) {
+                lhsValue = i;
+            }
+            if (RANK_NAMES_BY_WEIGHT[i] == rhs.get_rank()) {
+                rhsValue = i;
+            }
+        }
+        if (lhsValue < rhsValue) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
+    else {
+        for (int i = 0; i < NUM_SUITS; ++i) {
+            if (SUIT_NAMES_BY_WEIGHT[i] == lhs.get_suit()) {
+                lhsValue = i;
+            }
+            if (SUIT_NAMES_BY_WEIGHT[i] == rhs.get_suit()) {
+                rhsValue = i;
+            }
+        }
+        if (lhsValue < rhsValue) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
 }
 
 bool operator<=(const Card& lhs, const Card& rhs) {
+    int lhsValue = 0;
+    int rhsValue = 0;
+    if (lhs.get_rank() != rhs.get_rank()) {
+    
+        for (int i = 0; i < NUM_RANKS; ++i) {
+            if (RANK_NAMES_BY_WEIGHT[i] == lhs.get_rank()) {
+                lhsValue = i;
+            }
+            if (RANK_NAMES_BY_WEIGHT[i] == rhs.get_rank()) {
+                rhsValue = i;
+            }
+        }
+        if (lhsValue <= rhsValue) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
+    else {
+        for (int i = 0; i < NUM_SUITS; ++i) {
+            if (SUIT_NAMES_BY_WEIGHT[i] == lhs.get_suit()) {
+                lhsValue = i;
+            }
+            if (SUIT_NAMES_BY_WEIGHT[i] == rhs.get_suit()) {
+                rhsValue = i;
+            }
+        }
+        if (lhsValue <= rhsValue) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
 	return 0;
 
 }
 
 bool operator>(const Card& lhs, const Card& rhs) {
-	return 0;
+    int lhsValue = 0;
+    int rhsValue = 0;
+    if (lhs.get_rank() != rhs.get_rank()) {
+    
+        for (int i = 0; i < NUM_RANKS; ++i) {
+            if (RANK_NAMES_BY_WEIGHT[i] == lhs.get_rank()) {
+                lhsValue = i;
+            }
+            if (RANK_NAMES_BY_WEIGHT[i] == rhs.get_rank()) {
+                rhsValue = i;
+            }
+        }
+        if (lhsValue > rhsValue) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
+    else {
+        for (int i = 0; i < NUM_SUITS; ++i) {
+            if (SUIT_NAMES_BY_WEIGHT[i] == lhs.get_suit()) {
+                lhsValue = i;
+            }
+            if (SUIT_NAMES_BY_WEIGHT[i] == rhs.get_suit()) {
+                rhsValue = i;
+            }
+        }
+        if (lhsValue > rhsValue) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
 }
 
 bool operator>=(const Card& lhs, const Card& rhs) {
-	return 0;
+    int lhsValue = 0;
+    int rhsValue = 0;
+    if (lhs.get_rank() != rhs.get_rank()) {
+    
+        for (int i = 0; i < NUM_RANKS; ++i) {
+            if (RANK_NAMES_BY_WEIGHT[i] == lhs.get_rank()) {
+                lhsValue = i;
+            }
+            if (RANK_NAMES_BY_WEIGHT[i] == rhs.get_rank()) {
+                rhsValue = i;
+            }
+        }
+        if (lhsValue >= rhsValue) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
+    else {
+        for (int i = 0; i < NUM_SUITS; ++i) {
+            if (SUIT_NAMES_BY_WEIGHT[i] == lhs.get_suit()) {
+                lhsValue = i;
+            }
+            if (SUIT_NAMES_BY_WEIGHT[i] == rhs.get_suit()) {
+                rhsValue = i;
+            }
+        }
+        if (lhsValue >= rhsValue) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
 }
 
 bool operator==(const Card& lhs, const Card& rhs) {
-	return 0;
+    if ((lhs.get_rank() == rhs.get_rank()) && (lhs.get_suit() == rhs.get_suit())) {
+        return true;
+    }
+    else {
+        return false;
+    }
 
 }
 
 bool operator!=(const Card& lhs, const Card& rhs) {
-	return 0;
+    if ((lhs.get_rank() == rhs.get_rank()) && (lhs.get_suit() == rhs.get_suit())) {
+        return false;
+    }
+    else {
+        return true;
+    }
 
 }
 
 string Suit_next(const string& suit) {
-	return "";
+    if (suit == Card::SUIT_DIAMONDS) {
+        return Card::SUIT_HEARTS;
+    }
+    else if (suit == Card::SUIT_HEARTS) {
+        return Card::SUIT_DIAMONDS;
+    }
+    else if (suit == Card::SUIT_CLUBS) {
+        return Card::SUIT_SPADES;
+    }
+    else {
+        return Card::SUIT_CLUBS;
+    }
 	
 }
 
 ostream& operator<<(ostream& os, const Card& card) {
+    string rank = card.get_rank();
+    string suit = card.get_suit();
+    os << rank << " of " << suit;
 	return os;
+    
 }
 
 bool Card_less(const Card& a, const Card& b, const std::string& trump) {
+    //a is trump, b isn't
+    //b is trump, a isn't
+    //both trump: compare ranks
+    //neither trump, same suit: compare ranks
+    //neither trump, different suit, different rank: compare ranks
+    //neither trump, different suits, same rank: compare suit weight
+    
+    //a or b or both are trump
+    if (a.get_suit() == trump || b.get_suit() == trump) {
+        if (a.get_suit() == trump && b.get_suit() != trump) {
+            return false;
+            //a is not less than b
+        }
+        else if (b.get_suit() == trump && a.get_suit() != trump) {
+            return true;
+            //a is less than b
+        }
+        else if (b.get_suit() == trump && a.get_suit() == trump) {
+            bool lessThan = operator<(a, b);
+            if (lessThan) {
+                return true;
+            }
+            else {
+                return false;
+            }
 
-	return 0;
+        }
+    }
+    
+    //neither a nor b trump
+    bool lessThan = operator<(a, b);
+    if (lessThan) {
+        return true;
+    }
+    
+    else {
+        return false;
+    }
 }
 
+//trump suit > led suit > non-trump && non-led suit
 bool Card_less(const Card& a, const Card& b, const Card& led_card,
 	const std::string& trump) {
-	return 0;
+    string ledSuit = led_card.get_suit();
+    if (a.get_suit() == trump || b.get_suit() == trump) {
+        if (a.get_suit() == trump && b.get_suit() != trump) {
+            return false;
+            //a is not less than b
+        }
+        else if (b.get_suit() == trump && a.get_suit() != trump) {
+            return true;
+            //a is less than b
+        }
+        else if (b.get_suit() == trump && a.get_suit() == trump) {
+            bool lessThan = operator<(a, b);
+            if (lessThan) {
+                return true;
+            }
+            else {
+                return false;
+            }
+
+        }
+    }
+    //a is led, b is led
+    //a is is led, b is not led or trump
+    
+    else if ((a.get_suit() == ledSuit) || (b.get_suit() == ledSuit)) {
+        if (a.get_suit() == ledSuit && b.get_suit() != ledSuit) {
+            return false;
+            //a is not less than b
+        }
+        else if (b.get_suit() == ledSuit && a.get_suit() != ledSuit) {
+            return true;
+            //a is less than b
+        }
+        else if (b.get_suit() == ledSuit && a.get_suit() == ledSuit) {
+            bool lessThan = operator<(a, b);
+            if (lessThan) {
+                return true;
+            }
+            else {
+                return false;
+            }
+
+        }
+    }
+
+    //neither a nor b trump
+    bool lessThan = operator<(a, b);
+    if (lessThan) {
+        return true;
+    }
+    
+    else {
+        return false;
+    }
+    
 }
 
 
--- Player_tests.cpp
+++ Player_tests.cpp
@@ -25,7 +25,7 @@ TEST(test_player_get_name_2) {
 TEST(test_player_add_card_basic) {
     const Card c(Card::RANK_QUEEN, Card::SUIT_DIAMONDS);
     Player * frodo = Player_factory("Frodo", "Simple");
-    frodo->add_card(c);
+    frodo->add_card(Card(Card::RANK_TWO, Card::SUIT_SPADES));
     Card card_played = frodo->play_card(c, "Hearts");
     ASSERT_EQUAL(card_played, c);
     
@@ -110,7 +110,7 @@ TEST(test_player_make_trump_edgecase_2) {
     delete merry;
 }
 
-/*
+
 TEST(test_player_add_and_discard_1) {
     // Bob's hand
     Player * bob = Player_factory("Bob", "Simple");
@@ -123,10 +123,12 @@ TEST(test_player_add_and_discard_1) {
     
     bob->add_and_discard(Card(Card::RANK_NINE, Card::SUIT_HEARTS) // upcard
       );
-
-    
+    //ASSERT()
+    delete bob;
 }
-*/
+//create a test where upcard is taken
+
+//basic
 TEST(test_player_lead_card_basic) {
     // Bob's hand
     Player * bob = Player_factory("Bob", "Simple");
@@ -150,6 +152,141 @@ TEST(test_player_lead_card_basic) {
     delete bob;
 }
 
+//with trump cards
+TEST(test_player_lead_card_edgecase_1) {
+    // Bob's hand
+    Player * bob = Player_factory("Bob", "Simple");
+    bob->add_card(Card(Card::RANK_NINE, Card::SUIT_CLUBS));
+    bob->add_card(Card(Card::RANK_TEN, Card::SUIT_SPADES));
+    bob->add_card(Card(Card::RANK_QUEEN, Card::SUIT_SPADES));
+    bob->add_card(Card(Card::RANK_KING, Card::SUIT_SPADES));
+    bob->add_card(Card(Card::RANK_ACE, Card::SUIT_HEARTS));
+      // Bob adds a card to his hand and discards one card
+    
+    bob->add_and_discard(Card(Card::RANK_NINE, Card::SUIT_HEARTS) // upcard
+      );
+
+      // Bob leads
+    Card card_led = bob->lead_card(Card::SUIT_HEARTS);
+
+      // Verify the card Bob selected to lead
+    Card king_spades(Card::RANK_KING, Card::SUIT_SPADES);
+    ASSERT_EQUAL(card_led, king_spades); //check led card
+
+    delete bob;
+}
+
+//all trump cards
+TEST(test_player_lead_card_edgecase_2) {
+    // Bob's hand
+    Player * bob = Player_factory("Bob", "Simple");
+    bob->add_card(Card(Card::RANK_NINE, Card::SUIT_HEARTS));
+    bob->add_card(Card(Card::RANK_TEN, Card::SUIT_HEARTS));
+    bob->add_card(Card(Card::RANK_QUEEN, Card::SUIT_HEARTS));
+    bob->add_card(Card(Card::RANK_KING, Card::SUIT_HEARTS));
+    bob->add_card(Card(Card::RANK_ACE, Card::SUIT_HEARTS));
+      // Bob adds a card to his hand and discards one card
+    
+    bob->add_and_discard(Card(Card::RANK_NINE, Card::SUIT_HEARTS) // upcard
+      );
+
+      // Bob leads
+    Card card_led = bob->lead_card(Card::SUIT_HEARTS);
+
+      // Verify the card Bob selected to lead
+    Card ace_hearts(Card::RANK_ACE, Card::SUIT_HEARTS);
+    ASSERT_EQUAL(card_led, ace_hearts); //check led card
+
+    delete bob;
+}
+//all trumps except 1
+TEST(test_player_lead_card_edgecase_3) {
+    // Bob's hand
+    Player * bob = Player_factory("Bob", "Simple");
+    bob->add_card(Card(Card::RANK_NINE, Card::SUIT_HEARTS));
+    bob->add_card(Card(Card::RANK_TEN, Card::SUIT_HEARTS));
+    bob->add_card(Card(Card::RANK_TWO, Card::SUIT_CLUBS));
+    bob->add_card(Card(Card::RANK_KING, Card::SUIT_HEARTS));
+    bob->add_card(Card(Card::RANK_ACE, Card::SUIT_HEARTS));
+      // Bob adds a card to his hand and discards one card
+    
+    bob->add_and_discard(Card(Card::RANK_NINE, Card::SUIT_HEARTS) // upcard
+      );
+
+      // Bob leads
+    Card card_led = bob->lead_card(Card::SUIT_HEARTS);
+
+      // Verify the card Bob selected to lead
+    Card two_clubs(Card::RANK_TWO, Card::SUIT_CLUBS);
+    ASSERT_EQUAL(card_led, two_clubs); //check led card
+
+    delete bob;
+}
+
+//lead with right bower
+TEST(test_player_lead_card_edgecase_4) {
+    // Bob's hand
+    Player * bob = Player_factory("Bob", "Simple");
+    bob->add_card(Card(Card::RANK_NINE, Card::SUIT_HEARTS));
+    bob->add_card(Card(Card::RANK_TEN, Card::SUIT_HEARTS));
+    bob->add_card(Card(Card::RANK_QUEEN, Card::SUIT_HEARTS));
+    bob->add_card(Card(Card::RANK_JACK, Card::SUIT_HEARTS));
+    bob->add_card(Card(Card::RANK_ACE, Card::SUIT_HEARTS));
+      // Bob adds a card to his hand and discards one card
+    
+    bob->add_and_discard(Card(Card::RANK_NINE, Card::SUIT_HEARTS) // upcard
+      );
+
+      // Bob leads
+    Card card_led = bob->lead_card(Card::SUIT_HEARTS);
+
+      // Verify the card Bob selected to lead
+    Card jack_hearts(Card::RANK_JACK, Card::SUIT_HEARTS);
+    ASSERT_EQUAL(card_led, jack_hearts); //check led card
+
+    delete bob;
+}
+
+//test with 1 card, not trump
+TEST(test_player_lead_card_edgecase_5) {
+    // Bob's hand
+    Player * bob = Player_factory("Bob", "Simple");
+    bob->add_card(Card(Card::RANK_NINE, Card::SUIT_SPADES));
+      // Bob adds a card to his hand and discards one card
+    
+    bob->add_and_discard(Card(Card::RANK_NINE, Card::SUIT_HEARTS) // upcard
+      );
+
+      // Bob leads
+    Card card_led = bob->lead_card(Card::SUIT_HEARTS);
+
+      // Verify the card Bob selected to lead
+    Card nine_spades(Card::RANK_NINE, Card::SUIT_SPADES);
+    ASSERT_EQUAL(card_led, nine_spades); //check led card
+
+    delete bob;
+}
+
+//test with 1 card, pick up non-trump
+TEST(test_player_lead_card_edgecase_6) {
+    // Bob's hand
+    Player * bob = Player_factory("Bob", "Simple");
+    bob->add_card(Card(Card::RANK_NINE, Card::SUIT_HEARTS));
+      // Bob adds a card to his hand and discards one card
+    
+    bob->add_and_discard(Card(Card::RANK_NINE, Card::SUIT_SPADES) // upcard
+      );
+
+      // Bob leads
+    Card card_led = bob->lead_card(Card::SUIT_HEARTS);
+
+      // Verify the card Bob selected to lead
+    Card nine_spades(Card::RANK_NINE, Card::SUIT_SPADES);
+    ASSERT_EQUAL(card_led, nine_spades); //check led card
+
+    delete bob;
+}
+
 TEST(test_player_play_card_basic) {
     
 }
@@ -161,3 +298,4 @@ TEST(test_player_player_factory_basic) {
 // Add more tests here
 
 TEST_MAIN()
+//what does add and discard do, how to test it
