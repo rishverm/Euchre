c0a84663142ff7a663400a33628244599fb6c6dc
jelwing <jelwing@umich.edu>
2022-03-09 20:05:39 -0500

    commit

--- euchre.cpp
+++ euchre.cpp
@@ -10,65 +10,10 @@ using namespace std;
 
 
 
-class Game {
-private:
-    Player* players[4];
-    string shuffle;
-    //should this be 24?
-    Pack* pack;
-    int score[2];
-    string team[2];
-
-public:
-
-    Game(int argc, char* argv[]) {
-        int ind = 0;
-        for (int i = 4; i < argc; i += 2) {
-            players[ind] = Player_factory(argv[i], (argv[i + 1]));
-            ++ind;
-        }
-        shuffle = argv[2];
-
-        ifstream fin;
-        fin.open(argv[2]);
-        Pack gamePack(fin);
-        pack = &gamePack;
-        score[0] = 0;
-        score[1] = 0;
-        team[0] = players[0]->get_name() + " and " + players[2]->get_name();
-        team[1] = players[1]->get_name() + " and " + players[3]->get_name();
-    }
 
 
-    //is this a good way to store and create the players array?
-    static string cardType(Card& card) {
-        return card.get_rank() + " of " + card.get_suit();
-    }
-
-    static void deal_player(Player* player, Pack& pack, int number) {
-        for (int i = 0; i < number; i++) {
-            player->add_card(pack.deal_one());
-        }
-    }
-
-    void setUpTable();
-
-    void makingTrump();
-
-    void trickTaking();
-
-    void scoring();
-
-};
 
 
-#include "Player.h"
-#include <vector>
-#include <iostream>
-#include <fstream>
-#include "Pack.h"
-#include "Card.h"
-using namespace std;
 
 
 
@@ -76,12 +21,15 @@ class Game {
 private:
     Player* players[4];
     string shuffle;
-    //should this be 24?
+
     Pack* pack;
     int score[2];
     string team[2];
+    int hand;
 
 public:
+    
+    
 
     Game(int* argc, char* argv[]) {
         int ind = 0;
@@ -97,25 +45,84 @@ public:
         pack = &gamePack;
         score[0] = 0;
         score[1] = 0;
+        hand = 1;
         team[0] = players[0]->get_name() + " and " + players[2]->get_name();
         team[1] = players[1]->get_name() + " and " + players[3]->get_name();
     }
 
-    
 
-    Player* getPlayer1() {
+    static string cardType(Card& card) {
+        return card.get_rank() + " of " + card.get_suit();
+    }
+
+    void deal_player(Player* player, int number) {
+        for (int i = 0; i < number; i++) {
+            player->add_card(pack->deal_one());
+        }
+    }
+    
+    void deal(Player * dealer) {
+        
+        //dealer + 1 = 3 cards
+        deal_player(players[(dealer_index(hand) + 1) % 4], 3);
+        //dealer + 2 = 2 cards
+        deal_player(players[(dealer_index(hand) + 2) % 4], 2);
+        //dealer + 3 = 3 cards
+        deal_player(players[(dealer_index(hand) + 3) % 4], 3);
+        //dealer = 2 cards
+        deal_player(players[(dealer_index(hand) + 4) % 4], 2);
+        
+        //dealer + 1 = 2 cards
+        deal_player(players[(dealer_index(hand) + 1) % 4], 2);
+        //dealer + 2 = 3 cards
+        deal_player(players[(dealer_index(hand) + 2) % 4], 3);
+        //dealer + 3 = 2 cards
+        deal_player(players[(dealer_index(hand) + 3) % 4], 2);
+        //dealer = 3 cards
+        deal_player(players[(dealer_index(hand) + 4) % 4], 3);
+        
+        
+    }
+    
+    int dealer_index(int hand) {
+        return hand % 4;
+        //if hand = 0, player 0 deals
+        //if hand = 1, player 1 deals
+        // ...
+        //if hand = 4, player 0 deals
+    }
+
+    void setUpTable() {
+        if (shuffle == "shuffle") {
+            pack->shuffle();
+        }
+        
+        deal(players[dealer_index(hand)]);
+        //WE MIGHT NEED TO ITERATE HAND HERE
+        
+        cout << "Hand " << hand << endl;
+        cout << players[dealer_index(hand)]->get_name() << " deals" << endl;
+    }
+
+    void makingTrump();
+
+    void trickTaking();
+
+    void scoring();
+
+    Player* getPlayer0() {
         return players[0];
     }
 
-    Player* getPlayer2() {
+    Player* getPlayer1() {
         return players[1];
     }
 
-    Player* getPlayer3() {
+    Player* getPlayer2() {
         return players[2];
     }
 
-    Player* getPlayer4() {
+    Player* getPlayer3() {
         return players[3];
     }
 
@@ -129,6 +136,8 @@ public:
     }
 
     int* getScore() {
+        
+        ++hand;
         return score;
     }
 
@@ -137,24 +146,6 @@ public:
     }
 
 
-    //is this a good way to store and create the players array?
-    static string cardType(Card& card) {
-        return card.get_rank() + " of " + card.get_suit();
-    }
-
-    static void deal_player(Player* player, Pack& pack, int number) {
-        for (int i = 0; i < number; i++) {
-            player->add_card(pack.deal_one());
-        }
-    }
-
-    void setUpTable();
-
-    void makingTrump();
-
-    void trickTaking();
-
-    void scoring();
 
 };
 
@@ -220,17 +211,25 @@ int main(int argc, char* argv[]) {
     if (!file.is_open()) {
         cout << "Error opening " << argv[1] << endl;
     }
+    
+    for (int i = 0; i < argc; ++i) {
+        cout << argv[i] << " ";
+    }
+    
+    Game* x = new Game(&argc, argv);
+    cout << x->deal_player(, 3);
+    
+
+}
 
 
-
-
-
+/*
     Game* x = new Game(&argc, argv);
     if (x->getShuffle() == "shuffle") {
         x->getPack()->shuffle();
     }
 
     x->getPlayer1()->
-
+*/
 
 
